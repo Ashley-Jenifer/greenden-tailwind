50fb4360fd7bec15f3a439396898c5febbc4eb75
Ashley <ashleyjeyaseelan@gmail.com>
2024-02-21 06:39:27 +0530

    changes search of price and contact

--- contact.js
+++ contact.js
@@ -20,16 +20,16 @@ sendMessage.addEventListener("click", function() {
     var subject = document.getElementById("subject").value;
     var message = document.getElementById("message").value;
 
-    // Create a Blob with the message content
-    var blob = new Blob([`Email: ${email}\nSubject: ${subject}\nMessge: ${message}`], { type: "text/plain;charset=utf-8" });
-
-    // Create a temporary link element
-    var link = document.createElement("a");
-    link.href = URL.createObjectURL(blob);
-
-    // Set the file name
-    link.download = "message.doc";
-
-    // Simulate click on the link to trigger download
-    link.click();
-});
+    if(email !== "" && subject !== "" && message !== "") {
+        // If all fields are filled, proceed with creating and downloading the message
+        var blob = new Blob([`Email: ${email}\nSubject: ${subject}\nMessage: ${message}`], { type: "text/plain;charset=utf-8" });
+        var link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.download = "message.doc";
+        link.click();
+        alert("We will reach you soon !!!");
+    } else {
+        // If any field is empty, show an alert
+        alert("Please fill out all the fields.");
+    }
+})
--- product.js
+++ product.js
@@ -13,25 +13,22 @@ closenav.addEventListener("click",function()
     sidenav.style.right="-50%"
 })
 
-var productcontainer=document.getElementById("product-container")
-var search=document.getElementById("search")
-var productlist=productcontainer.querySelectorAll("div")
+var productcontainer = document.getElementById("product-container");
+var search = document.getElementById("search");
+var productlist = productcontainer.querySelectorAll("div");
 
-search.addEventListener("keyup",function()
-{
-    var enteredValue=event.target.value.toUpperCase()
+search.addEventListener("keyup", function(event) {
+    var enteredValue = event.target.value.toLowerCase(); 
+    for (var i = 0; i < productlist.length; i++) {
+        var productName = productlist[i].querySelector(".product-name").textContent.toLowerCase(); 
+        var productPriceText = productlist[i].querySelector(".price").textContent.toLowerCase(); 
 
-    for(count=0;count<productlist.length;count=count+1)
-    {
-        var productname=productlist[count].querySelector("h1").textContent
-    
-
-    if(productname.toUpperCase().indexOf(enteredValue)<0)
-    {
-        productlist[count].style.display="none"
-    } 
-    else{
-        productlist[count].style.display="block"
+        
+        if (productName.includes(enteredValue) || productPriceText.includes(enteredValue)) {
+            productlist[i].style.display = "block"; 
+        } else {
+            productlist[i].style.display = "none"; 
+        }
     }
-    }
-})
+});
+
