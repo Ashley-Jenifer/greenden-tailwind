329cd5bde08cc5adde79be253f59861ea59f0237
Ashley <ashleyjeyaseelan@gmail.com>
2024-03-04 12:46:01 +0530

    updated

--- contact.js
+++ contact.js
@@ -13,23 +13,33 @@ closenav.addEventListener("click",function()
     sidenav.style.right="-50%"
 })
 
-var sendMessage=document.getElementById("sendMessage")
+var sendMessage = document.getElementById("sendMessage");
+
 sendMessage.addEventListener("click", function() {
-    // Get input values
+    
     var email = document.getElementById("email").value;
     var subject = document.getElementById("subject").value;
     var message = document.getElementById("message").value;
 
-    if(email !== "" && subject !== "" && message !== "") {
-        // If all fields are filled, proceed with creating and downloading the message
-        var blob = new Blob([`Email: ${email}\nSubject: ${subject}\nMessage: ${message}`], { type: "text/plain;charset=utf-8" });
-        var link = document.createElement("a");
-        link.href = URL.createObjectURL(blob);
-        link.download = "message.doc";
-        link.click();
-        alert("We will reach you soon !!!");
+    
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (email !== "" && subject !== "" && message !== "") {
+        
+        if (emailPattern.test(email)) {
+            
+            var blob = new Blob([`Email: ${email}\nSubject: ${subject}\nMessage: ${message}`], { type: "text/plain;charset=utf-8" });
+            var link = document.createElement("a");
+            link.href = URL.createObjectURL(blob);
+            link.download = "message.doc";
+            link.click();
+            alert("We will reach you soon !!!");
+        } else {
+           
+            alert("Please enter a valid email address.");
+        }
     } else {
-        // If any field is empty, show an alert
+       
         alert("Please fill out all the fields.");
     }
-})
+});
